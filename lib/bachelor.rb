2ef1b14a3a977553cacc489efdf615e1c26b1088
SOYEONG OH <axionoso@gmail.com>
2019-03-28 04:42:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -39,6 +39,7 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
+  age_total = 0
   age_list = []
   data[season].each do |contestants, info|
     age_list << info["age"].to_f
