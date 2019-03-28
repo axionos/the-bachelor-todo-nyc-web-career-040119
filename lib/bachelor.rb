b696bf3b1b6911733c7c2d8c0b434c50c54d2479
SOYEONG OH <axionoso@gmail.com>
2019-03-28 04:07:04 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,5 @@
 def get_first_name_of_season_winner(data, season)
-  data[season]each do |seasons, info|
+  data[season].each do |seasons, info|
     if info["status"] == "Winner"
       info["name"].split(" ").pop.to_s
     end
