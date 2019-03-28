30947fddf36313947b44157a5ae56936184ea567
SOYEONG OH <axionoso@gmail.com>
2019-03-28 04:24:20 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -29,7 +29,9 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+
+  end
 end
 
 def get_average_age_for_season(data, season)
