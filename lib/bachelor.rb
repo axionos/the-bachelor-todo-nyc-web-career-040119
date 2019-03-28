525cc08e34d750561de960a535dec569de83b4b7
SOYEONG OH <axionoso@gmail.com>
2019-03-28 04:19:33 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -21,7 +21,7 @@ def count_contestants_by_hometown(data, hometown)
   data.each do |season, contestants|
     contestants.each do |contestant, info|
       if contestant["hometown"] == hometown
-
+        counter += 1
       end
     end
   end
